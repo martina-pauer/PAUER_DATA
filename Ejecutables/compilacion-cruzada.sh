5db8fc902dbee25a8df3efb2ba75290614b2a852
Martina Pauer <martinapauer38@gmail.com>
2025-04-02 02:43:12 +0000

    espero funcione

--- Ejecutables/compilacion-cruzada.sh
+++ Ejecutables/compilacion-cruzada.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 # Variables sys y arch para compilar a varios sistemas y arquitecturas multiples archivos en go
-sys="windows"
+sys="linux"
 arch="386"
 prefix=".."
-extension=".exe"
-compilados="categoric.go"
-ejecutables="windows/32-bits/"
+extension=""
+compilados="categoric$extension"
+ejecutables="Linux/32-bits/"
 # Compilo cada uno de los archivos
 GOOS=$sys GOARCH=$arch go build $prefix/categoric.go
 # Muevo ejecutables creados a su resppectiva carpeta
